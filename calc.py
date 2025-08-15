ba75e3b5aa6b4a5fa3c8e860b623f5eb4fb8e126
Kumar Ghimre <kumar485hyd@gmail.com>
2025-08-15 12:55:05 +0530

    Secondbra (#1)
    * added add funtion
    
    * added sub
    
    * multiply function
    
    * second branch
    
    * Update second.py
    
    * added comift before merge
    
    * Update second.py
    
    * added 4line

---
+++ calc.py
@@ -0,0 +1,13 @@
+def add(x,y):
+    return x + y
+
+def sub(x,y):
+    return x -y 
+
+def multiply(x,y):
+    return x * y
+
+
+
+result=add(10,20)
+print(result)
---
+++ second.py
@@ -0,0 +1,5 @@
+# this is second files
+# and second line
+#thrid line github
+#fourthline from cli
+#foruth line
